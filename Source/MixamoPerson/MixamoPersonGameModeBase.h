58fe786b34a5f0325cdbfc53f5a5d2db734d259a
Kochmarik Nikolay <kolyak2000@yandex.by>
2019-08-08 17:30:52 +0300

    ferst step

---
+++ Source/MixamoPerson/MixamoPersonGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MixamoPersonGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MIXAMOPERSON_API AMixamoPersonGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/MixamoPerson/Private/MasterCharacter.cpp
@@ -0,0 +1,98 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Public/MasterCharacter.h"
+#include "Engine.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+AMasterCharacter::AMasterCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>("Camera");
+	SpringArmComponent->SetupAttachment(RootComponent);
+	CameraComponent->SetupAttachment(SpringArmComponent);
+
+	SpringArmComponent->bUsePawnControlRotation = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMasterCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMasterCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AMasterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &AMasterCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AMasterCharacter::MoveRight);
+
+	PlayerInputComponent->BindAxis("LookUp", this, &AMasterCharacter::LookUp);
+	PlayerInputComponent->BindAxis("TurnRight", this, &AMasterCharacter::TurnRight);
+
+	PlayerInputComponent->BindAction("Jump", EInputEvent::IE_Pressed, this, &AMasterCharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", EInputEvent::IE_Released, this, &AMasterCharacter::StopJumping);
+
+}
+
+void AMasterCharacter::MoveForward(float Value)
+{
+	
+	if ((Controller != NULL) && (Value != 0.0f))
+	{		
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// get forward vector
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void AMasterCharacter::MoveRight(float Value)
+{
+	if ((Controller != NULL) && (Value != 0.0f))
+	{
+		// find out which way is right
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// get right vector 
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		// add movement in that direction
+		AddMovementInput(Direction, Value);
+		
+	}
+}
+
+void AMasterCharacter::TurnRight(float Value)
+{
+	if ((Controller != NULL) && (Value != 0.0f))
+	{
+		AddControllerYawInput(Value);
+	}
+}
+
+void AMasterCharacter::LookUp(float Value)
+{
+	if ((Controller != NULL) && (Value != 0.0f))
+	{
+		AddControllerPitchInput(Value);
+	}
+}
+
